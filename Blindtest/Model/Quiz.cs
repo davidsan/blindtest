b8ebfb1ca4f7f80b9b38fd82de46cdcee5b80987
David San <davidsanfr@gmail.com>
2014-10-12 11:00:22 +0200

    Add quiz model

--- Blindtest/Model/Quiz.cs
+++ Blindtest/Model/Quiz.cs
@@ -9,5 +9,26 @@ namespace Blindtest.Model
     class Quiz
     {
 
+        public HashSet<Song> Songs { get; set; }
+
+        public Song CorrectSong { get; set; }
+        public Quiz(int nrofSongs)
+        {
+            Songs = new HashSet<Song>();
+            SongManager sm = SongManager.Instance;
+            Random rnd = new Random();
+            while (Songs.Count < nrofSongs)
+            {
+                // draw a song from the song manager and add it to the set of songs
+                Song s = sm.Songs[rnd.Next(sm.Songs.Count)];
+                Songs.Add(s);
+            }
+
+            // pick the correct song among the set.
+            CorrectSong = null;
+        }
+
+
+
     }
 }
