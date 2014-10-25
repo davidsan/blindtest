651a46ce1b55e4ef55dcec51ce8ff854b276252f
Ieong David <ieong.david.id@gmail.com>
2014-10-25 02:03:04 +0200

    Add Settings button in title bar

---
+++ Blindtest/ViewModel/OptionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blindtest.View;
+using System.Windows.Input;
+using Blindtest.Service;
+using System.Threading;
+
+namespace Blindtest.ViewModel
+{
+    class OptionViewModel : ViewModelBase
+    {
+    }
+}
